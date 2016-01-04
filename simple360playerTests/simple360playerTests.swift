ca1a1aa5e4d8262b28badb58bb2854a989bffa01
Arthur Swiniarski <arthurswiniarski@macbook-pro-de-arthur-2.local>
2016-01-04 14:15:30 +0100

    Initial Commit

---
+++ simple360playerTests/simple360playerTests.swift
@@ -0,0 +1,36 @@
+//
+//  simple360playerTests.swift
+//  simple360playerTests
+//
+//  Created by Arthur Swiniarski on 04/01/16.
+//  Copyright © 2016 Arthur Swiniarski. All rights reserved.
+//
+
+import XCTest
+@testable import simple360player
+
+class simple360playerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
